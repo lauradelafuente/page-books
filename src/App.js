0d4f5c3868d4f5646484520ff222ab8a295a986a
laura de la fuente <laura.delafuente@outlook.com.br>
2025-08-02 14:19:09 -0300

    implementando search

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import Header from './components/header'
 import styled from 'styled-components'
+import Pesquisa from './components/search';
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
-li {
-    list-style: none;
-}`
+`
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/components/search/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    { nome: 'Dom Casmurro', id: 1, src: livro },
+    { nome: 'JS Assertivo', id: 2, src: livro },
+    { nome: 'A menina do lago', id: 3, src: livro },
+    { nome: 'Harry Potter', id: 4, src: livro }
+]
